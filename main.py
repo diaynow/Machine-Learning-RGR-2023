b9cd49b13cfa7e6a7eb5a96f54330bd034fc616d
Diana <ekspert.d@bk.ru>
2024-01-11 00:31:19 +0600

    rgr done

---
+++ main.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from PIL import Image
+from st_pages import Page, show_pages
+
+show_pages(
+    [
+        Page("webs/FirstWebSite.py", "Об авторе"),
+        Page("webs/SecondWebSite.py", "О датасете"),
+        Page("webs/ThirdWebSite.py", "Визуализация датасет"),
+        Page("webs/FourthWebSite.py", "Предсказание модели"),
+    ]
+)
+
+st.title('Расчетно-графическая работа по дисциплине "Машинное обучение"')
+st.header("Датасет с мошенническими транзакциями")
+
+st.image(
+    Image.open("photo_main.jpeg"),
+    width=450,
+)
